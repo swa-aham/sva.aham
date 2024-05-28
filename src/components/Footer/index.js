df6354f646c5eb62f8cbc0e745c7a30a4073e24f
Swa-aham <sohammandaviya@gmail.com>
2024-05-29 01:00:30 +0530

    email

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -88,7 +88,7 @@ function Footer() {
   return (
     <FooterContainer>
       <FooterWrapper>
-        <Logo>Soham Mandaviya</Logo>
+        <Logo>sohammandaviya@gmail.com</Logo>
         <Nav>
           <NavLink href="#about">About</NavLink>
           <NavLink href="#skills">Skills</NavLink>
