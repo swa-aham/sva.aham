5fc4a9ab25955ca1aa319f00a4d956d7ee1b7918
Swa-aham <sohammandaviya@gmail.com>
2024-05-29 00:48:14 +0530

    Update in contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -147,11 +147,11 @@ const Contact = () => {
         <Title>Contact</Title>
         <Desc>Feel free to reach out to me for any questions or opportunities!</Desc>
         <ContactForm ref={form} onSubmit={handleSubmit}>
-          <ContactTitle>Email Me 🚀</ContactTitle>
+          <ContactTitle>Let's Connect 🚀</ContactTitle>
           <ContactInput placeholder="Your Email" name="from_email" />
           <ContactInput placeholder="Your Name" name="from_name" />
           <ContactInput placeholder="Subject" name="subject" />
-          <ContactInputMessage placeholder="Message" rows="4" name="message" />
+          <ContactInputMessage placeholder="Start saying Hi..." rows="4" name="message" />
           <ContactButton type="submit" value="Send" />
         </ContactForm>
         <Snackbar
